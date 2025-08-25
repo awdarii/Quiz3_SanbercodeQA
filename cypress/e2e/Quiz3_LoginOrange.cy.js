70d7e411d62a1fde1ba2fe69340c880c5350a0a9
Ayu Wulandari <87927831+awdarii@users.noreply.github.com>
2025-08-25 15:27:06 +0700

    Cypress menggunakan intercept

--- cypress/e2e/Quiz3_LoginOrange.cy.js
+++ cypress/e2e/Quiz3_LoginOrange.cy.js
@@ -5,38 +5,56 @@ describe('OrangeHRM Login Tests', () => {
   });
 
   it('TC_001 - Login dengan username dan password valid', () => {
+    cy.intercept('POST', '**/auth/validate').as('loginRequest');
+
     cy.get('input[name="username"]').type('Admin');
     cy.get('input[name="password"]').type('admin123');
     cy.get('button[type="submit"]').click();
+
+    cy.wait('@loginRequest').its('response.statusCode').should('eq', 302);
 
     cy.url().should('include', '/dashboard'); // berhasil login
   });
 
   it('TC_002 - Login dengan username salah dan password benar', () => {
+    cy.intercept('POST', '**/auth/validate').as('loginRequest');
+
     cy.get('input[name="username"]').type('Adminn');
     cy.get('input[name="password"]').type('admin123');
     cy.get('button[type="submit"]').click();
+
+    cy.wait('@loginRequest').its('response.statusCode').should('eq', 302);
 
     cy.contains('Invalid credentials').should('be.visible');
   });
 
   it('TC_003 - Login dengan username benar dan password salah', () => {
+    cy.intercept('POST', '**/auth/validate').as('loginRequest');
+
     cy.get('input[name="username"]').type('Admin');
     cy.get('input[name="password"]').type('admin1234');
     cy.get('button[type="submit"]').click();
+
+    cy.wait('@loginRequest').its('response.statusCode').should('eq', 302);
 
     cy.contains('Invalid credentials').should('be.visible');
   });
 
   it('TC_004 - Login dengan username salah dan password salah', () => {
+    cy.intercept('POST', '**/auth/validate').as('loginRequest');
+
     cy.get('input[name="username"]').type('Adminn');
     cy.get('input[name="password"]').type('admin1234');
     cy.get('button[type="submit"]').click();
+
+    cy.wait('@loginRequest').its('response.statusCode').should('eq', 302);
 
     cy.contains('Invalid credentials').should('be.visible');
   });
 
   it('TC_005 - Login dengan username kosong dan password valid', () => {
+    cy.intercept('POST', '**/auth/validate').as('loginRequest');
+
     cy.get('input[name="password"]').type('admin123');
     cy.get('button[type="submit"]').click();
 
@@ -44,6 +62,8 @@ describe('OrangeHRM Login Tests', () => {
   });
 
   it('TC_006 - Login dengan username valid dan password kosong', () => {
+    cy.intercept('POST', '**/auth/validate').as('loginRequest');
+
     cy.get('input[name="username"]').type('Admin');
     cy.get('button[type="submit"]').click();
 
@@ -51,23 +71,33 @@ describe('OrangeHRM Login Tests', () => {
   });
 
   it('TC_007 - Login dengan username dan password kosong', () => {
+    cy.intercept('POST', '**/auth/validate').as('loginRequest');
+
     cy.get('button[type="submit"]').click();
 
     cy.get('.oxd-input-group > .oxd-text').should('contain', 'Required');
   });
 
   it('TC_008 - Login dengan spasi di username', () => {
+    cy.intercept('POST', '**/auth/validate').as('loginRequest');
+
     cy.get('input[name="username"]').type(' Admin ');
     cy.get('input[name="password"]').type('admin123');
     cy.get('button[type="submit"]').click();
+
+    cy.wait('@loginRequest').its('response.statusCode').should('eq', 302);
 
     cy.contains('Invalid credentials').should('be.visible');
   });
 
   it('TC_009 - Login dengan username/password yang salah berulang kali', () => {
+    cy.intercept('POST', '**/auth/validate').as('loginRequest');
+
     cy.get('input[name="username"]').type('Admin');
     cy.get('input[name="password"]').type('123');
     cy.get('button[type="submit"]').click();
+
+    cy.wait('@loginRequest').its('response.statusCode').should('eq', 302);
 
     cy.contains('Invalid credentials').should('be.visible');
   });
